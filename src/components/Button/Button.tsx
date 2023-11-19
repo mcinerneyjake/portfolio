5a01710a75ef8987fd23cf584b61ebb1c018ddb7
Jake McInerney <jakemcinerney1@gmail.com>
2023-11-18 23:07:33 -0600

    added react-aria button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,17 @@
+import { Button as AriaButton } from 'react-aria-components';
+
 type ButtonProps = {
   children: (string | number)[];
   onClick: () => void;
 };
 
-const Button = ({ children, onClick }: ButtonProps) => {
-  return (
-    <button className='text-xl font-medium text-white bg-black' onClick={onClick}>{children}</button>
-  );
-}
+const Button = ({ children, onClick }: ButtonProps) => (
+  <AriaButton
+    className='text-xl font-medium text-white bg-black'
+    onPress={onClick}
+  >
+    {children}
+  </AriaButton>
+);
 
 export default Button;
